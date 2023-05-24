26dcb3324fad8c2beff2d45751001236ae380b6a
nicetoeat <125798745+ycui1128@users.noreply.github.com>
2023-05-24 14:49:57 +0800

    Update tempalte.py

--- tempalte.py
+++ tempalte.py
@@ -15,16 +15,6 @@ from langchain.llms import OpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pypdf import PdfReader
 
-
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
-
-chain = load_chain()
-
-
 # Define a function to parse a PDF file and extract its text content
 @st.cache_data
 def parse_pdf(file: BytesIO) -> List[str]:
