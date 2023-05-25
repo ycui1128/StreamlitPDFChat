0f5bab2d22ee4d6776cfd9c4c69269de6f5a9b1f
cuiye <xiaoyezi1128@163.com>
2023-05-25 14:15:43 +0800

    hide menu

--- app.py
+++ app.py
@@ -89,6 +89,13 @@ st.sidebar.markdown(
 )
 st.sidebar.divider()
 
+hide_menu_style="""
+    <style>
+        #MainMenu {visibility:hidden;}
+        footer {visibility:hidden;}
+    </style>
+"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 api = st.sidebar.text_input(
                 "**Enter OpenAI API Key to talk with PDF**",
                 type="password",
